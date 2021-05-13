48eab51ec12f4ba10868d69ac5afe35b46f3f0c4
Rafael Renck <rafaelrenck@gmail.com>
2021-05-13 13:25:32 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="css/app.css" rel="stylesheet">
+
+    <title>.PONTO</title>
+  </head>
+  <body>
+  	<div class="container-xxl">
+		  <h1>Hello, world!</h1>
+		</div>
+
+
+    <script src="js/app.js"></script>
+  </body>
+</html>
